baade79d535f73528509bb2480883fb45e71da6e
Tomas Tornevall <thorne@tornevall.net>
2026-05-14 16:59:05 +0200

    Android Tools Project.

--- docs/socialgpt-chrome/js/shared/extension-links.js
+++
@@ -1,7 +0,0 @@
-(function (global) {
-    global.TNNetworksExtensionLinks = {
-        FORUM_URL: 'https://forum.tornevall.net',
-        TOOLS_SOCIAL_MEDIA_DASHBOARD_PATH: '/admin/social-media-tools/facebook',
-    };
-}(window));
-
